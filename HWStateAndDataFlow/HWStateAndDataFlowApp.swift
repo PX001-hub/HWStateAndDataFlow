70c42e118bd5822781975617901c02594e65d22b
Pttr <draivv@yandex.ru>
2022-11-07 00:25:46 +0300

    Initial Commit

---
+++ HWStateAndDataFlow/HWStateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWStateAndDataFlowApp.swift
+//  HWStateAndDataFlow
+//
+//  Created by P Z on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct HWStateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
